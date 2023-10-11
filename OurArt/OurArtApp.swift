8230fa68608486ef67b7243fd08a577575a6e023
ironnata <ironnata@gmail.com>
2023-10-11 14:18:05 +0200

    Initial Commit

---
+++ OurArt/OurArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  OurArtApp.swift
+//  OurArt
+//
+//  Created by Jongmo You on 11.10.23.
+//
+
+import SwiftUI
+
+@main
+struct OurArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
