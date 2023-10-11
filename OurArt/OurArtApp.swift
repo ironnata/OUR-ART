03cd1cd077ed1a8aeef617c43d9abea1c2617135
ironnata <ironnata@gmail.com>
2023-10-11 15:03:10 +0200

    Connect Firebase to Xcode

--- OurArt/OurArtApp.swift
+++ OurArt/OurArtApp.swift
@@ -6,12 +6,25 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct OurArtApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        print("Configured Firebase!")
+        
+        return true
     }
 }
