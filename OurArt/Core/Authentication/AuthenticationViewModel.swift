667b779a68c1b72d894f3b65df8035684ee5042e
ironnata <ironnata@gmail.com>
2023-10-22 19:57:22 +0200

    Adding encoder and decoder for userProfiles

--- OurArt/Core/Authentication/AuthenticationViewModel.swift
+++ OurArt/Core/Authentication/AuthenticationViewModel.swift
@@ -14,18 +14,21 @@ final class AuthenticationViewModel: ObservableObject {
         let helper = SignInGoogleHelper()
         let tokens = try await helper.signIn()
         let authDataResult = try await AuthenticationManager.shared.signInWithGoogle(tokens: tokens)
-        try await UserManager.shared.creatNewUser(auth: authDataResult)
+        let user = DBUser(userId: authDataResult.uid, isAnonymous: authDataResult.isAnonymous, email: authDataResult.email, photoUrl: authDataResult.photoUrl, dateCreated: Date())
+        try await UserManager.shared.creatNewUser(user: user)
     }
     
     func signInApple() async throws {
         let helper = SignInAppleHelper()
         let tokens = try await helper.startSignInWithAppleFlow()
         let authDataResult = try await AuthenticationManager.shared.signInWithApple(tokens: tokens)
-        try await UserManager.shared.creatNewUser(auth: authDataResult)
+        let user = DBUser(userId: authDataResult.uid, isAnonymous: authDataResult.isAnonymous, email: authDataResult.email, photoUrl: authDataResult.photoUrl, dateCreated: Date())
+        try await UserManager.shared.creatNewUser(user: user)
     }
     
     func signInAnonymous() async throws {
         let authDataResult = try await AuthenticationManager.shared.signInAnonymous()
-        try await UserManager.shared.creatNewUser(auth: authDataResult)
+        let user = DBUser(userId: authDataResult.uid, isAnonymous: authDataResult.isAnonymous, email: authDataResult.email, photoUrl: authDataResult.photoUrl, dateCreated: Date())
+        try await UserManager.shared.creatNewUser(user: user)
     }
 }
--- OurArt/Managers/UserManager.swift
+++ OurArt/Managers/UserManager.swift
@@ -9,7 +9,7 @@ import Foundation
 import FirebaseFirestore
 import FirebaseFirestoreSwift
 
-struct DBUser {
+struct DBUser: Codable {
     let userId: String
     let isAnonymous: Bool?
     let email: String?
@@ -22,36 +22,63 @@ final class UserManager {
     static let shared = UserManager()
     private init() { }
     
-    func creatNewUser(auth: AuthDataResultModel) async throws {
-        var userData: [String:Any] = [
-            "user_id" : auth.uid,
-            "is_anonymous" : auth.isAnonymous,
-            "date_created" : Timestamp(),
-            
-        ]
-        if let email = auth.email {
-            userData["email"] = email
-        }
-        if let photoUrl = auth.photoUrl {
-            userData["photo_url"] = photoUrl
-        }
-        
-        try await Firestore.firestore().collection("users").document(auth.uid).setData(userData, merge: false)
+    private let userCollection = Firestore.firestore().collection("users")
+    
+    private func userDocument(userId: String) -> DocumentReference {
+        userCollection.document(userId)
     }
     
-    func getUser(userId: String) async throws -> DBUser {
-        let snapshot = try await Firestore.firestore().collection("users").document(userId).getDocument()
-        
-        guard let data = snapshot.data(), let userId = data["user_id"] as? String else {
-            throw URLError(.badServerResponse)
-        }
-        
-        let isAnonymous = data["is_anonymous"] as? Bool
-        let email = data["email"] as? String
-        let photoUrl = data["photo_url"] as? String
-        let dateCreated = data["date_created"] as? Date
-        
-        return DBUser(userId: userId, isAnonymous: isAnonymous, email: email, photoUrl: photoUrl, dateCreated: dateCreated)
+    private let encoder: Firestore.Encoder = {
+        let encoder = Firestore.Encoder()
+        encoder.keyEncodingStrategy = .convertToSnakeCase
+        return encoder
+    }()
+    
+    private let decoder: Firestore.Decoder = {
+        let decoder = Firestore.Decoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        return decoder
+    }()
+    
+    func creatNewUser(user: DBUser) async throws {
+        try userDocument(userId: user.userId).setData(from: user, merge: false, encoder: encoder)
     }
+    
+//    func creatNewUser(auth: AuthDataResultModel) async throws {
+//        var userData: [String:Any] = [
+//            "user_id" : auth.uid,
+//            "is_anonymous" : auth.isAnonymous,
+//            "date_created" : Timestamp(),
+//            
+//        ]
+//        if let email = auth.email {
+//            userData["email"] = email
+//        }
+//        if let photoUrl = auth.photoUrl {
+//            userData["photo_url"] = photoUrl
+//        }
+//        
+//        try await userDocument(userId: auth.uid).setData(userData, merge: false)
+//    }
+    
+    func getUser(userId: String) async throws -> DBUser {
+        try await userDocument(userId: userId).getDocument(as: DBUser.self)
+    }
+    
+    
+//    func getUser(userId: String) async throws -> DBUser {
+//        let snapshot = try await userDocument(userId: userId).getDocument()
+//        
+//        guard let data = snapshot.data(), let userId = data["user_id"] as? String else {
+//            throw URLError(.badServerResponse)
+//        }
+//        
+//        let isAnonymous = data["is_anonymous"] as? Bool
+//        let email = data["email"] as? String
+//        let photoUrl = data["photo_url"] as? String
+//        let dateCreated = data["date_created"] as? Date
+//        
+//        return DBUser(userId: userId, isAnonymous: isAnonymous, email: email, photoUrl: photoUrl, dateCreated: dateCreated)
+//    }
     
 }
