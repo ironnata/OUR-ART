12804d46c8e4aa30881c8a831178c5f10b1a6d50
ironnata <ironnata@gmail.com>
2023-11-15 21:58:02 +0100

    creating ProfileEditView

---
+++ OurArt/Core/Profile/ProfileEditView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileEditView.swift
+//  OurArt
+//
+//  Created by Jongmo You on 15.11.23.
+//
+
+import SwiftUI
+
+struct ProfileEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileEditView()
+}
